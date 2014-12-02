6d991504bef47aa2d4ba480ef693a6563fb32024
masaki muto <m.muto0128@gmail.com>
2014-12-02 10:34:49 +0900

    [add]koscil, multi

---
+++ SynthTree/Unit/ConstantOscillator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthTree.Unit
+{
+	public class ConstantOscillator : Unit1In2Out
+	{
+		public double Constant;
+		public double Phase;
+
+		public override void Update()
+		{
+			base.Update();
+			Out1.Value = Constant;
+			Out2.Value = Math.Sin(In.Value + Phase);
+		}
+	}
+}
---
+++ SynthTree/Unit/Multiplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthTree.Unit
+{
+	public class Multiplier : Unit2In1Out
+	{
+		public override void Update()
+		{
+			base.Update();
+			Out.Value = In1.Value * In2.Value;
+		}
+	}
+}
