6678f9951c857db53e4eb7c66718d0c993a8f946
Masaki Muto <m.muto0128@gmail.com>
2014-12-13 21:06:14 +0900

    [clean]separate options clas[clean]separate options classs

---
+++ SynthTree/Util/KeyValueSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthTree.Util
+{
+	public class KeyValueSet<TKey, TValue>  : IEnumerable<KeyValuePair<TKey, TValue>>
+	{
+		List<KeyValuePair<TKey, TValue>> items;
+
+		public KeyValueSet()
+		{
+			items = new List<KeyValuePair<TKey, TValue>>();
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			items.Add(new KeyValuePair<TKey, TValue>(key, value));
+		}
+
+
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		{
+			return items.GetEnumerator();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return items.GetEnumerator();
+		}
+	}
+}
